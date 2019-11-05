06ebf09a5640be15afe61163454888a92ed8cf1c
tawfiqsiam <toto.siam999@gmail.com>
2019-11-05 14:02:05 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -107,7 +107,15 @@ client.on("guildMemberAdd", member => {
 });
     
  
-
+member.guild.fetchInvites().then(guildInvites => {
+      const ei = guildInvites[member.guild.id];
+      const invite = guildInvites.find(i => ei.get(i.code).uses < i.uses);
+      const inviter = client.users.get(invite.inviter.id);
+       welcomer.send(`<@${member.user.id}> تمت الدعوه من <@${inviter.id}>`);
+    }); 
+  });
+     })
+    }})
 
 
 client.login(process.env.BOT_TOKEN);
